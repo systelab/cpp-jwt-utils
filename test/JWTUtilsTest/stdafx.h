a95cbb141b17ee60ca0b2161ec57362576916a2c
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-02-22 18:52:32 +0100

    Treated stdafx.h as lower case

--- test/JWTUtilsTest/stdafx.h
+++ test/JWTUtilsTest/stdafx.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows 10 or later
-#define _WIN32_WINNT 0x0A00	// Change this to the appropriate value to target other versions of Windows.
-#endif						
-
 #include <stdio.h>
 #include <tchar.h>
 

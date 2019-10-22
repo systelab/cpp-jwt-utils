6c3e74b1b6ee771e381179ec46bfa571fb8b88d9
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-22 21:33:53 +0200

    Updated main to be multiplatform
    Also configured gtest verbosity

--- test/JWTUtilsTest/JWTUtilsTest.cpp
+++ test/JWTUtilsTest/JWTUtilsTest.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 
 
-int _tmain(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
+	::testing::FLAGS_gmock_verbose = "error";
 	::testing::InitGoogleTest(&argc, argv);
 
 	int rc = RUN_ALL_TESTS();

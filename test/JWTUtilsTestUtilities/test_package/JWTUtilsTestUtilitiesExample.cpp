6c292ffdd204f541e1b9cc05cb466a5a4b1d93d3
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-22 23:13:34 +0200

    Added test package for JWTUtilsTestUtilities

---
+++ test/JWTUtilsTestUtilities/test_package/JWTUtilsTestUtilitiesExample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "JWTUtilsTestUtilities/Mocks/MockTokenBuilderService.h"
+
+
+int main(int argc, char *argv[])
+{
+	systelab::jwt::test_utility::MockTokenBuilderService tokenBuilderService;
+
+    return 0;
+}

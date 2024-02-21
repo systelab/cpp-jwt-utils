1631a6e1e7b5174e85ec319b499eec1d62c3b031
Thymir <davidcarcamo4@gmail.com>
2024-02-21 18:07:02 +0100

    Rescue conan test files

---
+++ test/JWTUtilsTestUtilities/test_package/conanfile.py
@@ -0,0 +1,26 @@
+from conans import ConanFile, CMake
+
+
+class JWTUtilsTestUtilitiesTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_find_package"
+    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g", "1.1.1k"]}
+    default_options = {"gtest":"1.10.0", "openssl":"1.1.1k"}
+
+    def configure(self):
+        self.options["JWTUtilsTestUtilities"].gtest = self.options.gtest
+        self.options["JWTUtilsTestUtilities"].openssl = self.options.openssl
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+
+    def test(self):
+        cmake = CMake(self)
+        cmake.test()
